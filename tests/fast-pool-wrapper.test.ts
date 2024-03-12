0d955a8113b5a88c950c1ef41e0257d52c62f0b1
friedger <mail@friedger.de>
2024-03-12 11:45:26 +0100

    chore: add log of events

--- tests/fast-pool-wrapper.test.ts
+++ tests/fast-pool-wrapper.test.ts
@@ -32,12 +32,13 @@ describe("fast pool wrapper tests", () => {
 
     // delegate-stx on fast pool
     // this includes delegate-stack-stx and maybe aggregate commit
-    const result = simnet.callPublicFn(
+    const response = simnet.callPublicFn(
       contracts.wrapper,
       "delegate-stx",
       [Cl.uint(10_000_000e6)],
       address1
-    ).result as ResponseOkCV<
+    );
+    const result = response.result as ResponseOkCV<
       TupleCV<{
         "commit-result": BooleanCV;
         "lock-result": TupleCV<{
@@ -50,6 +51,13 @@ describe("fast pool wrapper tests", () => {
     expect(cvToString(result)).toBe(
       "(ok (tuple (commit-result true) (lock-result (tuple (lock-amount u9999999000000) (stacker ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.fast-pool-wrapper) (unlock-burn-height u4200)))))"
     );
+
+    expect(response.events[0].event).toBe("stx_transfer_event");
+
+    // Can I expect locking events here?
+    console.log(response.events);
+
+    // Do I need to advance the chain to see the correct numbers of stx-account?
     simnet.mineEmptyBlocks(1);
 
     // get stx-account for stacker
